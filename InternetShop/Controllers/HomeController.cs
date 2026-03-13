a8d6d0f942d25c86f71eea6d750dcd4ebc9e51d5
Veber_gs03@top-academy.ru <Veber_gs03@top-academy.ru>
2026-03-13 22:05:29 +0500

    Базовая структура интеренет-магазина с HomeController и Views

---
+++ InternetShop/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternetShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
